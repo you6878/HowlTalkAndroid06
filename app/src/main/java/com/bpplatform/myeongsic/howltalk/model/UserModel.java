93e654ca5a298b9863232495968ce0ad26204a5b
MyeongSic <you6878@icloud.com>
2017-09-04 20:27:38 +0900

    init

---
+++ app/src/main/java/com/bpplatform/myeongsic/howltalk/model/UserModel.java
@@ -0,0 +1,10 @@
+package com.bpplatform.myeongsic.howltalk.model;
+
+/**
+ * Created by myeongsic on 2017. 8. 14..
+ */
+
+public class UserModel {
+    public String userName;
+    public String profieImageUrl;
+}
